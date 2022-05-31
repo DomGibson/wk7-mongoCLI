1454ad4c66759d3f2469a7a2ea8e2ce26db988be
DomGibson <103586347+DomGibson@users.noreply.github.com>
2022-05-31 11:37:31 +0100

    Update index.js

--- mongo-db-thing/src/utils/index.js
+++ mongo-db-thing/src/utils/index.js
@@ -11,3 +11,18 @@ exports.addMovie = async (movieObj, collection) => {
     const movies = await collection.find().toArray();
     console.log(movies);
   };
+
+  exports.findMovie = async (filter, collection) => {
+    const result = await collection.findOne(filter);
+    console.log(result);
+  };
+
+  exports.updateMovie = async (filter, updateObj, collection) => {
+    const updateRes = await collection.updateOne(filter, {$set: updateObj});
+    if (updateRes.modifiedCount > 0) {
+    } else {
+    console.log("Nothing Updated");
+    }
+  };
+
+  exports.deleteMovie = async (filter, collection)
