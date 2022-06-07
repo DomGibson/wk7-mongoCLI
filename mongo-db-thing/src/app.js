258d50a7520a8f11b1f2264cf3b45e19ec7a72cc
DomGibson <103586347+DomGibson@users.noreply.github.com>
2022-06-07 10:45:44 +0100

    Update app.js

--- mongo-db-thing/src/app.js
+++ mongo-db-thing/src/app.js
@@ -1,6 +1,6 @@
 const yargs = require("yargs");
 const { client, connection } = require("./db/connection");
-const { addMovie, listMovies } = require("./utils");
+const { addMovie, listMovies, findMovie, deleteMovie, updateMovie } = require("./utils");
 
 const app = async (yargsObj) => {
   const collection = await connection();
@@ -8,10 +8,26 @@ const app = async (yargsObj) => {
     if (yargsObj.add) {
       //add movie to mongodb
       await addMovie({ title: yargsObj.title }, collection);
-    } else if (yargsObj.list) {
+    }
+    
+    else if (yargsObj.list) {
       //list movies from mongodb
       await listMovies(collection);
-    } else {
+    }
+    
+    else if (yargsObj.find) {
+      await findMovie({title: yargsObj.title, actor: yargsObj.actor},collection)
+    }
+
+    else if (yargsObj.updateMovie) {
+      await updateMovie({title: yargsObj.title, newTitle: yargsObj.newTitle},collection);
+    }
+
+    else if (yargsObj.delete) {
+      await deleteMovie({title:yargsObj.title}, collection) /* Delete movie function */
+    }
+    
+    else {
       console.log("Incorrect command");
     }
   } catch (error) {
@@ -21,3 +37,9 @@ const app = async (yargsObj) => {
 };
 
 app(yargs.argv);
+
+// node src/app.js --add --title "John Wick"
+// node src/app.js --list
+// node src/app.js --del --title "John Wick"
+// node src/app.js --updt --title "John Wick" --newTitle "John Wick 2" Updates Actor
+// node src/app.js --find --title "John Wick" --actor "Keanu Reeves"
